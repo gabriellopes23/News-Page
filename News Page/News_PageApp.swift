9cd79362693a7421c83afeb8259d433d864deed6
Gabriel Lopes <lopessantosgabriel@outlook.com>
2024-05-16 14:19:27 -0300

    Initial Commit

---
+++ News Page/News_PageApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_PageApp.swift
+//  News Page
+//
+//  Created by Gabriel Lopes on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct News_PageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
